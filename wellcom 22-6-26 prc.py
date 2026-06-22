5eb79a63dda8a74ea4163f1b9ec50b655b84ad22
hananafzal809 <hananafzal245@gmail.com>
2026-06-22 06:46:50 -0700

    wellcome 22-6

---
+++ wellcom 22-6-26 prc.py
@@ -0,0 +1,18 @@
+print("Wellcome to practic for 22 june 2026.")
+PropertyID=545
+print(type(PropertyID))
+PropertyPrice=234.2
+print(PropertyPrice)
+print(type(PropertyPrice))
+propertyArea="DHA"
+print(propertyArea)
+print(type(propertyArea))
+PropertyPrice2=(PropertyPrice*5)
+print(PropertyPrice2)
+print(type(PropertyPrice2))
+PropertyCity=input("Enter your city DHA")
+print(PropertyCity)
+PropertyInto=input ("Please enter your into:",)
+n=len(PropertyInto)
+print(n)
+                    
