f4d256f5dc5b5bca42a322a42d0022c3b73262b9
hananafzal809 <hananafzal245@gmail.com>
2026-06-24 06:40:42 -0700

    2nd files data stuture

---
+++ practice data stucture tuples.py
@@ -0,0 +1,10 @@
+booktuples = (454,'think and grow','good works', '789.8',9090)
+print(booktuples)
+print(type(booktuples))
+print("total len in booktuples:",len(booktuples))
+for i in booktuples :
+    print(i)
+    print(booktuples[3])
+
+
+
