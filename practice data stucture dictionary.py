54270e7170026e22cd7076c5651f5e4cc3b1ea9e
hananafzal809 <hananafzal245@gmail.com>
2026-06-24 07:00:03 -0700

    4th files data stuture

---
+++ practice data stucture dictionary.py
@@ -0,0 +1,11 @@
+bookdictionary = {
+    "mango": "apple" ,
+    "anything": 455.5
+}
+print(bookdictionary)
+print(type(bookdictionary))
+print("total len in bookdictionary:",len(bookdictionary))
+for i in bookdictionary:
+    print (bookdictionary[i])
+for i in bookdictionary:
+    print(i)
